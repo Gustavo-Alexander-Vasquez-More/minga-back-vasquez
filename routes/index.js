513cc5287d53d310d47c2a6d0bf7e0022107d7ff
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-06-26 19:05:28 -0300

    minga-08 (#7)

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-//import category_router from './categories.js';
+import category_router from './categories.js';
 import author_router from './authors.js';
 import manga_router from './mangas.js';
 import chapter_router from './chapters.js';
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.use('/authors', author_router) //obligo al enrutador principal a usar las rutas con lapalabrita /authors
-//router.use('/categories', category_router)
+router.use('/categories', category_router)
 router.use('/mangas',manga_router)
 router.use('/chapters',chapter_router)
 router.use('/users', user_router)
