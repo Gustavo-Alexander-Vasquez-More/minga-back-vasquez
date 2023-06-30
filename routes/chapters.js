c094c430773d8fdc5466e60ed2971afb1a3bc5db
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-06-30 18:48:51 -0300

    m04-sin-passport

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import read from "../controllers/chapters/read.js";
+import create from "../controllers/chapters/create.js";
 const chapter_router=Router()
 //chapter_router.post()
 chapter_router.get('/', read)
+chapter_router.post('/', create)
 //chapter_router.post()
 //chapter_router.delete()
 export default chapter_router
