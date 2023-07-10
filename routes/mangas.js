edc783dc56c4a0122cece7c9b59e6a43f3602357
AnaJazVazquez27 <vazquezanajazmin27@gmail.com>
2023-07-10 10:34:07 -0300

    cambio jaz

--- routes/mangas.js
+++ routes/mangas.js
@@ -3,11 +3,12 @@ import read  from "../controllers/mangas/read.js"
 import create from "../controllers/mangas/create.js"
 import validator from "../middlewares/validator.js"
 import mangaValidation from "../schemas/Manga/mangaVal.js"
+import mangaExists from "../middlewares/mangaExists.js"
 /*import is_property_of from "../middlewares/is_property_of.js"*/
 
 const manga_router=Router()
 manga_router.get('/', read)
-manga_router.post('/mangaC',validator(mangaValidation), create);
+manga_router.post('/mangaC',validator(mangaValidation), mangaExists,create);
 //manga_router.put()
 //manga_router.delete()
 export default manga_router
