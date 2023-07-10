a52b41f097882318f43ecfb27631ce48fc0718d7
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-07-09 23:55:16 -0300

    correccion2

--- routes/chapters.js
+++ routes/chapters.js
@@ -6,10 +6,11 @@ import validadorChapter from "../schemas/chapter/validadorChapter.js";
 import validator from "../middlewares/validator.js";
 import is_property_of from "../middlewares/is_property_of.js";
 import passport from "../middlewares/passport.js";
+import exists_order from "../middlewares/exists_order.js";
 
 
 const chapter_router=Router()
 
 chapter_router.get('/', passport.authenticate('jwt',{session:false}),  read)
-chapter_router.post('/chapterC',addCoverPhotoMiddleware,validator(validadorChapter),is_property_of, create)
+chapter_router.post('/chapterC',addCoverPhotoMiddleware,validator(validadorChapter),is_property_of, exists_order , create)
 export default chapter_router
